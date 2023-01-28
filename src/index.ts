371d87c5037b0719c47d0983cf97b7b2316309f8
mattlockyer <mattdlockyer@gmail.com>
2023-01-27 18:49:59 -0800

    mvp done

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// 	'64YjVg24ekJq7fqofsb8hjipbVBdfsx5TiVqcGLnERTqtAvoKZvWDbjFsTRq7evX7pcvYrHGvJzJGh98zNLEZA6d',
+// 	'3nfRxPdEMihVrJzW6bA7jYxxTSCUbXoHJ7UxoV7xKsT2Q39eLrViYj5UPxjW817gfCRhnWwZdPNaZietANj5aErN',
+// 	'4yrwrbm1vhJdyaeNX64eFF6M63LUTL9vwba4jAv9mNX6Hzk7D96Tpp3irZb3QszwvQ3bJvFYis77Ec2cEtUDQ8nb',
+// 	'4WPTzU3Zjui1oaaKUjPTDFcNyjV95N3bfreUVa3RLWaKMfpdpGtHfBuEnTfrhB92H9rU6yynZoLsU94E2wZY1FLk',
+// 	'3icCZAKHmhrYx2L2dofzDNbnoMXd19tNoq78P4SSj6q9xk7NP9Gg5kdC7RnZJiqkE51GMQYjYns7qqjNdWRspLpA',
+// 	'2ZuHJgKzZwkpmejERsxosdP3t8XyifdHNAPKup2q4yAL4zxQXjfjpXGUKTeY9g9ViW7rgDDW5NZXa9nNbyc64GFY',
+// 	'aCcdMBRwBLUxoeG3ZtSQDwEP1He3mDmdkk5BHihnwhZWurPRSLzGaf77zrsydZJfjGTuHVSixFYRSXsiTCV6BHH'
 
-import { initKeypom, getEnv } from 'keypom-js';
+// trick keypom so near-api-js isn't using window and browser key store
+process.versions.node = 'foo';
+
+import { initKeypom, claim, getDropInformation, parseNearAmount } from 'keypom-js';
+import { NFTStorage, File } from 'nft.storage'
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -21,6 +23,8 @@ export interface Env {
 	// MY_BUCKET: R2Bucket;
 }
 
+const REQUIRED_DEPOSIT = parseNearAmount('0.1')
+
 export default {
 	async fetch(
 		request: Request,
@@ -28,12 +32,45 @@ export default {
 		ctx: ExecutionContext
 	): Promise<Response> {
 
+		const { searchParams } = new URL(request.url)
+		const secretKey = searchParams.get('secretKey');
+
 		await initKeypom({
 			network: 'testnet',
 		})
 
-		const { near } = getEnv();
+		let dropInfo = null
+		try {
+			dropInfo = await getDropInformation({
+				secretKey: secretKey!,
+			})
+		} catch (e) {
+			return new Response(JSON.stringify({ error: 'invalid drop' }));
+		}
 
-		return new Response(JSON.stringify(near));
+		const enough = dropInfo!.deposit_per_use === REQUIRED_DEPOSIT
+		if (!enough) {
+			return new Response(JSON.stringify({ error: 'drop too small' }));
+		}
+
+		let claimed = false
+		try {
+			const response = await claim({
+				secretKey: secretKey!,
+				accountId: 'md1.testnet'
+			})
+			claimed = response[0].status.SuccessValue === ''
+		} catch(e) {
+			console.warn(e)
+		}
+
+		if (!claimed) {
+			return new Response(JSON.stringify({ error: 'drop not claimed' }));
+		}
+		
+		const client = new NFTStorage({ token: env.NFT_API_KEY })
+		let cid = await client.storeBlob(new Blob(["Hello World!"]))
+		return new Response(JSON.stringify(`https://ipfs.io/ipfs/${cid}`));
+
 	},
 };
